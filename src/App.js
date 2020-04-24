70808f72abdc514a4ef8bfd3d10e505762ac6146
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-23 23:44:03 -0500

    created the employee table file with logic

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { Component } from "react";
-import EmployeeCard from "./components/EmployeeCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+import React from "react";
 import employees from "./employees.json";
+import EmployeeTable from "./components/EmployeeTable"
 
-class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    employees
-  };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Employee List</Title>
-        {this.state.employees.map(employee => (
-          <EmployeeCard
-            id={employee.id}
-            key={employee.id}
-            name={employee.name}
-            occupation={employee.occupation}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+export default function App() {
+  return (
+    <EmployeeTable />
+  )
 }
 
-export default App;
---
+++ src/components/EmployeeTable/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function EmployeeTable(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Occupation</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
