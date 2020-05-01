8417a49577cfc32e2751c3872aa95b6bbc5ef970
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-30 20:22:54 -0500

    completed the server side part

--- server/app.js
+++ server/app.js
@@ -2,9 +2,15 @@ const path = require('path');
 const express = require('express')
 
 const app = express();
-const port = process.env.PORT || 3000
+// const publicPath  = (__dirname, '..', 'public');
+const port = process.env.PORT || 3001
 
-app.use(express.static(publicPath))
+app.use(express.static(path.join(__dirname, '..' , 'build')));
 
 
-app.get
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'build', '..','index.html'))
+})
+app.listen(port, () => {
+    console.log(`server listening on ${port} 👂`)
+})
