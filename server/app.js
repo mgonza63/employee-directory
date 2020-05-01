33bfbbf08eb5063debe5a920a313110c8b85cafe
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-30 21:52:41 -0500

    heroku fix

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const express = require('express')
+const express = require('express');
 
 const app = express();
 // const publicPath  = (__dirname, '..', 'public');
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname, '..' , 'build')));
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', '..','index.html'))
 })
 app.listen(port, () => {
